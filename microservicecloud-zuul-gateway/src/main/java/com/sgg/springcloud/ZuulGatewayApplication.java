1144491a87580fc1d23ad1b20a6fec7f5a6fdb28
tc_tc_tc <455433520@qq.com>
2019-10-20 14:37:18 +0800

    zuul

---
+++ microservicecloud-zuul-gateway/src/main/java/com/sgg/springcloud/ZuulGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.sgg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Auther: tianchao
+ * @Date: 2019/10/20 11:39
+ * @Description:
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
